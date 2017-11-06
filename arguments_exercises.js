dd15d175d4f30948e63b7c6a02e18b0342ba1e26
jacob <jbutler8@terpmail.umd.edu>
2017-11-06 15:44:00 -0800

    complete JS currying practice

--- arguments_exercises.js
+++ arguments_exercises.js
@@ -1,4 +1,4 @@
-// function sum(){
+// // function sum(){
 //   let args = Array.from(arguments);
 //   let total = 0;
 //
@@ -9,8 +9,7 @@
 //   return total;
 // }
 
-
-function sum(...nums){
+function restSum(...nums){
   let total = 0;
 
   nums.forEach((num) => {
@@ -30,7 +29,7 @@ Function.prototype.myBind = function(context){ // breakfast
   };
 };
 
-Function.prototype.myBind2 = function(context, ...args){ //breakfast, "meow"
+Function.prototype.myRestBind = function(context, ...args){ //breakfast, "meow"
   const that = this; //says
 
   return function bound(...calltimeArgs){ //Markov
@@ -40,4 +39,72 @@ Function.prototype.myBind2 = function(context, ...args){ //breakfast, "meow"
 
 Function.prototype.myShortBind = function(context, ...args){
   return (...calltimeArgs) => this.apply(context, args.concat(calltimeArgs));
+};
+
+function curriedSum(numArgs){
+  let numbers = [];
+
+  function _curriedSum(num) {
+    numbers.push(num);
+    if (numbers.length === numArgs) {
+      let total = 0;
+
+      numbers.forEach(function(n){
+        total += n;
+      });
+
+      return total;
+
+    } else {
+      return _curriedSum;
+    }
+  }
+  return _curriedSum;
+}
+
+
+// this = method we're trying to call (says)
+// context = where we want to call it (breakfast)
+// curry.apply(breakfast, args)
+
+Function.prototype.applyCurriedSum = function curry(context, numArgs){
+  let args = Array.from(arguments);
+  let numbers = [];
+
+  function _curriedSum(num){
+    numbers.push(num);
+    let total = 0;
+
+    if (numbers.length === numArgs){
+      numbers.forEach(function(n){
+        total += n;
+      });
+      return total;
+    } else {
+      return _curriedSum;
+    }
+  }
+
+  return _curriedSum.apply(context, args.join(numArgs));
+};
+
+Function.prototype.spreadCurriedSum = function curry(numArgs, ...args) {
+  let numbers = [];
+
+  function _curriedSum(num) {
+    numbers.push(num);
+    if (numbers.length === numArgs) {
+      let total = 0;
+
+      numbers.forEach(function(n){
+        total += n;
+      });
+
+      return total;
+
+    } else {
+      return _curriedSum;
+    }
+  }
+  return _curriedSum;
 };
