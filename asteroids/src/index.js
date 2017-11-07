bcd00ae5ac22dafb419067ada7eabe29be35804b
jacob <jbutler8@terpmail.umd.edu>
2017-11-06 17:34:57 -0800

    Init asteroids project

---
+++ asteroids/src/index.js
@@ -0,0 +1,13 @@
+import _ from 'lodash';
+
+
+function component() {
+
+  var element = document.createElement('div');
+
+  element.innerHTML = _.join(['Hello', 'Webpack'], ' ');
+
+  return element;
+}
+
+document.body.appendChild(component());
